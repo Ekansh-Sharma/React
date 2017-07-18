6ba7b7e1d0eba9e3025d2a40b9267c312dd4dca2
user <ekanshsharmacse11@gmail.com>
2017-07-18 14:45:04 +0530

    Form submit.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {name: '', fruit: 'mango'};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event){
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+  handleSubmit(event){
+    console.log('Entered name is ::'+ this.state.name + 'and Fruit is ::'+ this.state.fruit);
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name: <input name='name' value={this.state.name} onChange={this.handleChange} />
+        </label>
+        <select name='fruit' value={this.state.fruit} onChange={this.handleChange}>
+          <option value='grapefruit'>Grapefruit</option>
+          <option value='lime'>Lime</option>
+          <option value="coconut">Coconut</option>
+          <option value="mango">Mango</option>
+        </select>
+        <input type='submit' value='submit' />
+      </form>
     );
   }
 }
